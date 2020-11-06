e448eb91dd33dfa0022e7524285fce60fc7fc92e
jfmekker <main@jacobmekker.com>
2020-11-06 11:29:04 -0800

    Refactored ritual circles slightly.

--- src/main/java/com/jacobmekker/aurumancy/rituals/RitualCircle.java
+++ src/main/java/com/jacobmekker/aurumancy/rituals/RitualCircle.java
@@ -12,37 +12,47 @@ import javax.annotation.Nullable;
 
 public class RitualCircle {
 
+    private static final int OVERWORLD_CIRCLE_SIZE = 3;
+    private static final int NETHER_CIRCLE_SIZE = 6;
+    private static final int END_CIRCLE_SIZE = 9;
+
+    private static final int OVERWORLD_CIRCLE_MAX_PILLARS = 4;
+    private static final int NETHER_CIRCLE_MAX_PILLARS = 8;
+    private static final int END_CIRCLE_MAX_PILLARS = 16;
+
+    private static final int OVERWORLD_CIRCLE_PILLAR_HEIGHT = 2;
+    private static final int NETHER_CIRCLE_PILLAR_HEIGHT = 4;
+    private static final int END_CIRCLE_PILLAR_HEIGHT = 6;
+
     public static @Nullable CirclePower GetCirclePower(World world, BlockPos center) {
         CirclePower power = new CirclePower();
-        power.add(GetCircleOverworldPower(world, center));
-        power.add(GetCircleNetherPower(world, center.add(0,-1,0)));
-        power.add(GetCircleEndPower(world, center.add(0,-2,0)));
+        power.add(GetCircleOverworldPower(world, center.add(0,-1,0)));
+        power.add(GetCircleNetherPower(world, center.add(0,-2,0)));
+        power.add(GetCircleEndPower(world, center.add(0,-3,0)));
         return (power.total() == 0) ? null : power;
     }
 
     private static @Nullable CirclePower GetPillarOverworldPower(World world, BlockPos bottom) {
-        int height = 2;
         CirclePower pillar = new CirclePower();
 
         // Check pillar material
         int i = 1;
         Block material = null;
-        while (i <= height) {
+        while (i <= OVERWORLD_CIRCLE_PILLAR_HEIGHT) {
             Block block = world.getBlockState(bottom.add(0,i,0)).getBlock();
             material = material == null ? block : material;
             if (material != block) return null;
 
-            if (block instanceof FenceBlock)
-                pillar.add(0,0,0);
-            else if (block == Blocks.IRON_BLOCK)
-                pillar.add(2,0,0);
-            else if (block == Blocks.OBSIDIAN)
-                pillar.add(5,0,0);
-            else
-                return null;
-
             i += 1;
         }
+        if (material instanceof FenceBlock)
+            pillar.add(0,0,0);
+        else if (material == Blocks.IRON_BLOCK)
+            pillar.add(2,0,0);
+        else if (material == Blocks.OBSIDIAN)
+            pillar.add(5,0,0);
+        else
+            return null;
 
         // Check pillar top
         Block block = world.getBlockState(bottom.add(0,i,0)).getBlock();
@@ -65,29 +75,26 @@ public class RitualCircle {
     private static @Nullable CirclePower GetCircleOverworldPower(World world, BlockPos center) {
         CirclePower circle = new CirclePower(1,0,0);
 
-        int size = 3, num_pillars = 0;
-        for (int i = -size; i <= size ; i++) {
-            for (int j = -size; j <= size; j++) {
+        int num_pillars = 0;
+        for (int i = -OVERWORLD_CIRCLE_SIZE; i <= OVERWORLD_CIRCLE_SIZE ; i++) {
+            for (int j = -OVERWORLD_CIRCLE_SIZE; j <= OVERWORLD_CIRCLE_SIZE; j++) {
                 Block block = world.getBlockState(center.add(i,0,j)).getBlock();
 
-                // Must be stone bricks except:
-                //   the center block
-                //   pillars on coal block
-                if (block == Blocks.STONE_BRICKS || (i == 0 && j == 0)) {
-                    continue;
-                }
-                if (block == Blocks.COAL_BLOCK) {
-                    // Check pillar
-                    if (num_pillars < 4) {
-                        CirclePower pillar = GetPillarOverworldPower(world, center.add(i,0,j));
-                        if (pillar == null) return null;
-                        circle.add(pillar);
-                        num_pillars += 1;
+                // Must be stone bricks except pillars on coal block
+                if (block != Blocks.STONE_BRICKS) {
+                    if (block == Blocks.COAL_BLOCK) {
+                        // Check pillar
+                        if (num_pillars < OVERWORLD_CIRCLE_MAX_PILLARS) {
+                            CirclePower pillar = GetPillarOverworldPower(world, center.add(i,0,j));
+                            if (pillar == null) return null;
+                            circle.add(pillar);
+                            num_pillars += 1;
+                        }
+                        else return null;
                     }
-                    else return null;
-                }
-                else {
-                    return null;
+                    else {
+                        return null;
+                    }
                 }
             }
         }
@@ -96,28 +103,26 @@ public class RitualCircle {
     }
 
     private static @Nullable CirclePower GetPillarNetherPower(World world, BlockPos bottom) {
-        int height = 4;
         CirclePower pillar = new CirclePower();
 
         // Check pillar material
         int i = 1;
         Block material = null;
-        while (i <= height) {
+        while (i <= NETHER_CIRCLE_PILLAR_HEIGHT) {
             Block block = world.getBlockState(bottom.add(0,i,0)).getBlock();
             material = material == null ? block : material;
             if (material != block) return null;
 
-            if (block == Blocks.NETHERRACK)
-                pillar.add(0,1,0);
-            else if (block == Blocks.QUARTZ_BLOCK) // TODO: final?
-                pillar.add(0,2,0);
-            else if (block == Blocks.PRISMARINE_BRICKS)
-                pillar.add(0,5,0);
-            else
-                return null;
-
             i += 1;
         }
+        if (material == Blocks.NETHERRACK)
+            pillar.add(0,0,0);
+        else if (material == Blocks.QUARTZ_BLOCK) // TODO: final?
+            pillar.add(0,2,0);
+        else if (material == Blocks.PRISMARINE_BRICKS)
+            pillar.add(0,5,0);
+        else
+            return null;
 
         // Check pillar top
         Block block = world.getBlockState(bottom.add(0,i,0)).getBlock();
@@ -140,29 +145,26 @@ public class RitualCircle {
     private static @Nullable CirclePower GetCircleNetherPower(World world, BlockPos center) {
         CirclePower circle = new CirclePower(0,1,0);
 
-        int size = 6, num_pillars = 0;
-        for (int i = -size; i <= size ; i++) {
-            for (int j = -size; j <= size; j++) {
+        int num_pillars = 0;
+        for (int i = -NETHER_CIRCLE_SIZE; i <= NETHER_CIRCLE_SIZE ; i++) {
+            for (int j = -NETHER_CIRCLE_SIZE; j <= NETHER_CIRCLE_SIZE; j++) {
                 Block block = world.getBlockState(center.add(i,0,j)).getBlock();
 
-                // Must be nether bricks except:
-                //   the center block
-                //   pillars on soul sand
-                if (block == Blocks.NETHER_BRICKS || (i == 0 && j == 0)) {
-                    continue;
-                }
-                if (block == Blocks.SOUL_SAND) {
-                    // Check pillar
-                    if (num_pillars < 8) {
-                        CirclePower pillar = GetPillarNetherPower(world, center.add(i,0,j));
-                        if (pillar == null) return null;
-                        circle.add(pillar);
-                        num_pillars += 1;
+                // Must be nether bricks except pillars on soul sand
+                if (block != Blocks.NETHER_BRICKS) {
+                    if (block == Blocks.SOUL_SAND) {
+                        // Check pillar
+                        if (num_pillars < NETHER_CIRCLE_MAX_PILLARS) {
+                            CirclePower pillar = GetPillarNetherPower(world, center.add(i,0,j));
+                            if (pillar == null) return null;
+                            circle.add(pillar);
+                            num_pillars += 1;
+                        }
+                        else return null;
                     }
-                    else return null;
-                }
-                else {
-                    return null;
+                    else {
+                        return null;
+                    }
                 }
             }
         }
@@ -182,23 +184,22 @@ public class RitualCircle {
             material = material == null ? block : material;
             if (material != block) return null;
 
-            if (block == Blocks.END_STONE)
-                pillar.add(0,0,1);
-            else if (block == Blocks.PURPUR_PILLAR)
-                pillar.add(0,0,2);
-            else if (block instanceof ShulkerBoxBlock)
-                pillar.add(0,0,5);
-            else
-                return null;
-
             i += 1;
         }
+        if (material == Blocks.END_STONE)
+            pillar.add(0,0,0);
+        else if (material == Blocks.PURPUR_PILLAR)
+            pillar.add(0,0,2);
+        else if (material instanceof ShulkerBoxBlock)
+            pillar.add(0,0,5);
+        else
+            return null;
 
         // Check pillar top
         Block block = world.getBlockState(bottom.add(0,i,0)).getBlock();
         if (block == Blocks.AIR)
             pillar.add(0,0,0);
-        else if (block == Blocks.FIRE)
+        else if (block == Blocks.DIAMOND_BLOCK) // TODO ender block?
             pillar.add(0,0,1);
         else if (block == Blocks.END_ROD)
             pillar.add(0,0,2);
@@ -215,29 +216,26 @@ public class RitualCircle {
     private static @Nullable CirclePower GetCircleEndPower(World world, BlockPos center) {
         CirclePower circle = new CirclePower(0,0,1);
 
-        int size = 9, num_pillars = 0;
-        for (int i = -size; i <= size ; i++) {
-            for (int j = -size; j <= size; j++) {
+        int num_pillars = 0;
+        for (int i = -END_CIRCLE_SIZE; i <= END_CIRCLE_SIZE ; i++) {
+            for (int j = -END_CIRCLE_SIZE; j <= END_CIRCLE_SIZE; j++) {
                 Block block = world.getBlockState(center.add(i,0,j)).getBlock();
 
-                // Must be end stone bricks except:
-                //   the center block
-                //   pillars on obsidian
-                if (block == Blocks.END_STONE_BRICKS || (i == 0 && j == 0)) {
-                    continue;
-                }
-                if (block == Blocks.OBSIDIAN) {
-                    // Check pillar
-                    if (num_pillars < 16) {
-                        CirclePower pillar = GetPillarEndPower(world, center.add(i,0,j));
-                        if (pillar == null) return null;
-                        circle.add(pillar);
-                        num_pillars += 1;
+                // Must be end stone bricks except pillars on obsidian
+                if (block != Blocks.END_STONE_BRICKS) {
+                    if (block == Blocks.OBSIDIAN) {
+                        // Check pillar
+                        if (num_pillars < END_CIRCLE_MAX_PILLARS) {
+                            CirclePower pillar = GetPillarEndPower(world, center.add(i,0,j));
+                            if (pillar == null) return null;
+                            circle.add(pillar);
+                            num_pillars += 1;
+                        }
+                        else return null;
                     }
-                    else return null;
-                }
-                else {
-                    return null;
+                    else {
+                        return null;
+                    }
                 }
             }
         }
