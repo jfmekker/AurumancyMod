55cc3463cc7bcdd0d01e1dad325e28f85728472b
Jacob Mekker <main@jacobmekker.com>
2020-12-18 00:41:14 -0800

    Fixed Scrying Cube texture.

--- src/main/java/com/jacobmekker/aurumancy/blocks/ScryingCubeBlock.java
+++ src/main/java/com/jacobmekker/aurumancy/blocks/ScryingCubeBlock.java
@@ -5,6 +5,7 @@ import com.jacobmekker.aurumancy.blocks.tileentities.ScryingCubeTileEntity;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.SoundType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
@@ -29,7 +30,7 @@ public class ScryingCubeBlock extends Block {
     }
 
     public ScryingCubeBlock(Properties properties) {
-        super(properties);
+        super(properties.sound(SoundType.GLASS).lightValue(5));
     }
 
     @Override
